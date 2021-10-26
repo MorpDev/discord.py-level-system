4ff1fb95e5f651f8d64436ec3d7c71dce0131f53
MorpDev <89537270+MorpDev@users.noreply.github.com>
2021-10-26 18:23:28 +0200

    Create level-system.py

---
+++ level-system.py
@@ -0,0 +1,62 @@
+@client.event
+async def on_member_join(member):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot == False:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+
+        await update_data(users, message.author)
+        await add_experience(users, message.author, 5)
+        await level_up(users, message.author, message)
+
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+
+    await client.process_commands(message)
+
+
+async def update_data(users, user):
+    if not f'{user.id}' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['tecrube'] = 0
+        users[f'{user.id}']['level'] = 1
+
+
+async def add_tecrube(users, user, exp):
+    users[f'{user.id}']['tecrube'] += exp
+
+
+async def level_up(users, user, message):
+    with open('levels.json', 'r') as g:
+        levels = json.load(g)
+    experience = users[f'{user.id}']['tecrube']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(tecrube ** (1 / 4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} Seviye Atladın! Yeni Seviyen {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
+
+@client.command()
+async def level(ctx, member: discord.Member = None):
+    if not member:
+        id = ctx.message.author.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'Senin Seviyen {lvl}!')
+    else:
+        id = member.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'{member} Seviyesi {lvl}!')
